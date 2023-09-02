2a855f25d5e417ca29a2ad7552e5916bebe33da8
8rupees <122161229+8rupees@users.noreply.github.com>
2023-09-02 17:27:08 +0530

    Comment fix chatlux.py

--- main/chatlux.py
+++ main/chatlux.py
@@ -61,7 +61,7 @@ def svr(PORT):
 
 def clt(PORT):
     HOST = '127.0.0.1'
-    #PORT = 9993
+    #PORT = 9995
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
